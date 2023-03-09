9d53fbc296105d19455f0852e5ddbe929d2155d1
NN <qwertylollel@gmail.com>
2023-03-09 20:31:23 +0600

    InMemoryDatabase
    Took 1 hour 7 minutes

--- src/main/java/com/nuracell/bs/BsApplication.java
+++ src/main/java/com/nuracell/bs/BsApplication.java
@@ -1,12 +1,26 @@
 package com.nuracell.bs;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner clr() {
+		return args -> {
+			System.out.println("Hello from BsApplication.java again arrre");
+//			ApplicationContext appContext = new AnnotationConfigApplicationContext();
+//			System.out.println("appContext.getApplicationName() = " + appContext.getApplicationName());
+//			System.out.println("appContext.getDisplayName() = " + appContext.getDisplayName());
+//			System.out.println("appContext.getBean(\"clr\") = " + appContext.getBean("clr"));
+		};
 	}
 }
--- src/main/java/com/nuracell/bs/security/SecurityConfiguration.java
+++ src/main/java/com/nuracell/bs/security/SecurityConfiguration.java
@@ -1,11 +1,23 @@
 package com.nuracell.bs.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Base64;
@@ -13,23 +25,65 @@ import java.util.Base64;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+
                 .authorizeHttpRequests()
                     .requestMatchers("/", "/api/**")
                     .permitAll()
                     .requestMatchers("/admin/**")
                     .hasAnyRole("admin")
+                    .anyRequest() // any other request must be authenticated (optional)
+                    .authenticated()
+
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .authenticationProvider(authenticationProvider())
                 .httpBasic();
 
-        System.out.println(encodedBasicAuth("user", "5c0d02f4-9d69-4e9f-8dc3-2e67098b51f5"));
-
+        System.out.println("encoded basic auth: " + encodedBasicAuth("user", "5c0d02f4-9d69-4e9f-8dc3-2e67098b51f5"));
+        System.out.println("authProvider: " + authenticationProvider());
+        System.out.println("authProvider: ");
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        System.out.println("authenticationProvider");
+        return daoAuthenticationProvider;
+    }
+
+    // InMemoryDataBase
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails roxana = User.builder()
+                .username("roxana")
+                .password(passwordEncoder.encode("roxana"))
+                .roles("ADMIN")
+                .build();
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(admin, roxana);
+        System.out.println("userDetailsService" + inMemoryUserDetailsManager.loadUserByUsername("admin"));
+        return inMemoryUserDetailsManager;
     }
 
     //
@@ -50,4 +104,8 @@ public class SecurityConfiguration {
 
         return theEncodedString.toString();
     }
+
+
+
+
 }
