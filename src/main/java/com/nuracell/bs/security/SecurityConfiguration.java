a8798369e006364c797894b7ef30d8598f62f7dc
Qwww Rere <qwertylollel@gmail.com>
2023-03-27 19:54:38 +0600

    sending 401 instead of 403
    Took 13 minutes

--- src/main/java/com/nuracell/bs/security/SecurityConfiguration.java
+++ src/main/java/com/nuracell/bs/security/SecurityConfiguration.java
@@ -5,6 +5,9 @@ import com.nuracell.bs.entity.AppUser;
 import com.nuracell.bs.exception.CustomAccessDeniedHandler;
 import com.nuracell.bs.repository.AppUserRepository;
 import com.nuracell.bs.service.AppUserDetailsService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +20,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,12 +28,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
 import java.util.Base64;
 
 @Configuration
@@ -47,7 +53,6 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-//                .httpBasic().disable()
 
                 .authorizeHttpRequests()
                     .requestMatchers("/", "/api/**", "/auth/**", "/cookie/**", "/security/**")
@@ -55,16 +60,9 @@ public class SecurityConfiguration {
                     .anyRequest()
                     .hasAnyRole("ADMIN")
 
-//                .and()
-//                    .httpBasic()
-
-//                .and()
-//                    .formLogin()
-//                    .failureHandler()
-//                    .successHandler()
-
                 .and()
                     .exceptionHandling()
+                    .authenticationEntryPoint(http401UnauthorizedEntryPoint())
                     .accessDeniedHandler(accessDeniedHandler())
 
                 .and()
@@ -107,5 +105,17 @@ public class SecurityConfiguration {
     @Bean
     public AccessDeniedHandler accessDeniedHandler() {
         return new CustomAccessDeniedHandler();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint http401UnauthorizedEntryPoint() {
+        return new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
+                }
+            }
+        };
     }
 }
